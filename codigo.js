665fb056a8c0696f804cba2e7b128eb6151f5743
Rodrigofray <100170017+Rodrigofray@users.noreply.github.com>
2022-03-21 22:28:42 -0300

    Add files via upload

---
+++ codigo.js
@@ -0,0 +1,12 @@
+function soma() {
+    var valor1 = document.getElementById("valor1").value;
+
+    var valor2 = document.getElementById("valor2").value;
+    
+    var soma = +valor1 + +valor2;
+
+    var valor2 = document.getElementById("resultado");
+
+    resultado.value = soma
+   
+}
